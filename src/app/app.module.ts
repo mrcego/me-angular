3015ef1421b17065732828c7968dececce806dfa
Cesar <cesar_gomez_h@hotmail.com>
2019-01-13 16:29:49 -0500

    Implemented skeleton via template to Angular template compatibility

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftComponent } from './left/left.component';
+import { RightComponent } from './right/right.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftComponent,
+    RightComponent
   ],
   imports: [
     BrowserModule,
