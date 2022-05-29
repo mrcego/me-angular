82ff68e64204ccc0fcb472623cfe860601505361
Cesar Gomez <cesargomezh90@gmail.com>
2022-05-29 01:42:35 -0500

    upgrade: Angular 9

--- src/main.server.ts
+++ src/main.server.ts
@@ -7,3 +7,4 @@ import { enableProdMode } from '@angular/core';
     
     export {AppServerModule} from './app/app.server.module';
     
+export { renderModule, renderModuleFactory } from '@angular/platform-server';
